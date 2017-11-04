7a2e3fce0507ae1a1cec47942b1bba9586bb13c8
mswindu <windu.whr@gmail.com>
2017-11-04 21:35:53 +0700

    Добавил unit тесты для структуры данных Stack

--- src/test/java/com/nilov/algorithms/data_structures/StackTest.java
+++ src/test/java/com/nilov/algorithms/data_structures/StackTest.java
@@ -1,10 +1,19 @@
 package com.nilov.algorithms.data_structures;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import java.util.EmptyStackException;
 import java.util.Random;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class StackTest {
+
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
 
     private static final Random RANDOM = new Random();
     private static final int SIZE = 10000;
@@ -20,11 +29,38 @@ public class StackTest {
     }
 
     @Test
-    public void testStack() throws Exception {
+    public void testPopEmptyStackException() throws Exception {
+        thrown.expect(EmptyStackException.class);
         Stack stack = new Stack();
+        stack.pop();
+    }
 
-        for (int aData : data) {
-            stack.push(aData);
+    @Test
+    public void testPeekEmptyStackException() throws Exception {
+        thrown.expect(EmptyStackException.class);
+        Stack stack = new Stack();
+        stack.peek();
+    }
+
+    @Test
+    public void testPushAndPop() throws Exception {
+        Stack stack = new Stack();
+        int element;
+        for (int aData : data) stack.push(aData);
+
+        assertTrue(!stack.isEmpty());
+
+        int index = data.length - 1;
+
+        while (!stack.isEmpty()) {
+            element = stack.pop();
+            assertEquals(data[index], element);
+            index--;
         }
+
+        thrown.expect(EmptyStackException.class);
+        element = stack.pop();
+
+        assertTrue(stack.isEmpty());
     }
 }
